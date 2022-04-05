d5d58109d52593e1e6e89307a93abedb84c28685
fellahi-ali <fellahi.ali@gmail.com>
2022-04-05 17:42:34 +0100

    ch3. external code finished

---
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/src/modules/my_app.py
@@ -0,0 +1,3 @@
+# importing and using a custom module
+import my_module
+my_module.multiply(5, 9)
---
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/src/modules/my_module.py
@@ -0,0 +1,3 @@
+# define a function that print the result of multiplication of 2 numbers
+def multiply(x, y):
+    print(f'{x} * {y} = {x * y}') # usage of string formating
