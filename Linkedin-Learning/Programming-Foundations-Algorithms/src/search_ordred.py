b9a0d2ca3c2c7063182f80276a19dba9dee3c0ac
fellahi-ali <fellahi.ali@gmail.com>
2022-04-12 16:14:24 +0100

    search in ordred list (todo)

---
+++ Linkedin-Learning/Programming-Foundations-Algorithms/src/search_ordred.py
@@ -0,0 +1,6 @@
+# This algo uses binnary search, and it has complexity of
+# O(log(n))
+def find_item(item, items):
+    pass
+    # todo: #12 implement search algo in ordered list using binnary search
+    
