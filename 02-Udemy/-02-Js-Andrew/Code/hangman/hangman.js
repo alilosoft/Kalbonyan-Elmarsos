a9fef75d7a5e09435252b852376b3fd0aac0f43b
Ali Fellahi <fellahi.ali@gmail.com>
2022-09-20 10:50:47 +0100

    #18 hangman class

--- 02-Udemy/-02-Js-Andrew/Code/hangman/hangman.js
+++ 02-Udemy/-02-Js-Andrew/Code/hangman/hangman.js
@@ -1,52 +1,54 @@
-const Hangman = function (word, maxAttempts) {
-  this.status = 'playing'
-  this.word = word.toLowerCase().split('')
-  this.guess = []
-  this.attempts = maxAttempts
-}
+class Hangman {
+  constructor(word, maxAttempts) {
+    this.status = 'playing'
+    this.word = word.toLowerCase().split('')
+    this.guess = []
+    this.attempts = maxAttempts
+  }
 
-Hangman.prototype.showPuzzle = function (display = console.log) {
-  let puzzle = ''
-  this.word.forEach((letter) => {
-    if (letter === ' ' || this.guess.includes(letter)) {
-      puzzle += letter
-    } else {
-      puzzle += '*'
-    }
-  })
-  display(puzzle)
-  return puzzle
-}
-
-Hangman.prototype.makeGuess = function (letter) {
-  //todo: validate input
-  if (this.status === 'playing') {
-    const guess = letter.toLowerCase()
-    if (!this.guess.includes(guess)) {
-      this.guess.push(guess)
-      if (!this.word.includes(guess)) {
-        this.attempts--
-        if (this.attempts === 0) {
-          this.status = 'failed'
-        }
+  showPuzzle(display = console.log) {
+    let puzzle = ''
+    this.word.forEach((letter) => {
+      if (letter === ' ' || this.guess.includes(letter)) {
+        puzzle += letter
       } else {
-        const finished = this.word.every(
-          (letter) => letter === ' ' || this.guess.includes(letter)
-        )
-        if (finished) {
-          this.status = 'finished'
+        puzzle += '*'
+      }
+    })
+    display(puzzle)
+    return puzzle
+  }
+
+  makeGuess(letter) {
+    //todo: validate input
+    if (this.status === 'playing') {
+      const guess = letter.toLowerCase()
+      if (!this.guess.includes(guess)) {
+        this.guess.push(guess)
+        if (!this.word.includes(guess)) {
+          this.attempts--
+          if (this.attempts === 0) {
+            this.status = 'failed'
+          }
+        } else {
+          const finished = this.word.every(
+            (letter) => letter === ' ' || this.guess.includes(letter)
+          )
+          if (finished) {
+            this.status = 'finished'
+          }
         }
       }
     }
   }
-}
 
-Hangman.prototype.showResult = function (display = console.log) {
-  if (this.status === 'playing') {
-    display(`Playing...🤔 (${this.attempts} attempts left)`)
-  } else if (this.status === 'finished') {
-    display('Congratulations 🎉')
-  } else {
-    display(`Game Over 😭 (Mysterious word was: "${this.word.join('')}")`)
+  showResult(display = console.log) {
+    if (this.status === 'playing') {
+      display(`Playing...🤔 (${this.attempts} attempts left)`)
+    } else if (this.status === 'finished') {
+      display('Congratulations 🎉')
+    } else {
+      display(`Game Over 😭 (Mysterious word was: "${this.word.join('')}")`)
+    }
   }
 }
