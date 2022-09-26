dc405f62536e67380e741c84bcea43c6e42ddaed
Ali Fellahi <fellahi.ali@gmail.com>
2022-09-26 19:41:03 +0100

    #18 more async/await examples

--- 02-Udemy/-02-Js-Andrew/Code/async/async-await.js
+++ 02-Udemy/-02-Js-Andrew/Code/async/async-await.js
@@ -13,3 +13,34 @@ console.log(helloPromise('alilo'))
 helloPromise('alilo').then(console.log).catch(console.error)
 
 helloPromise(123).then(console.log).catch(console.error)
+
+const calcSquare = (num) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof num === 'number') {
+        resolve(num * num)
+      } else {
+        reject('Please provide a number')
+      }
+    }, 500)
+  })
+
+// async/await in action
+const showSquares4Times = async (num) => {
+  let square = await calcSquare(num)
+  console.log(`${num} x ${num} = ${square}`)
+
+  num = square
+  square = await calcSquare(square)
+  console.log(`${num} x ${num} = ${square}`)
+
+  num = square
+  square = await calcSquare(square)
+  console.log(`${num} x ${num} = ${square}`)
+
+  num = square
+  square = await calcSquare('square')
+  console.log(`${num} x ${num} = ${square}`)
+}
+
+showSquares4Times(2).catch(console.error)
