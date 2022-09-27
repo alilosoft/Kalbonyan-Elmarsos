c19a996775f60472a996a1c179173be1375aafe0
Ali Fellahi <fellahi.ali@gmail.com>
2022-09-27 14:10:58 +0100

    #18 more async/await practice

--- 02-Udemy/-02-Js-Andrew/Code/async/requests.js
+++ 02-Udemy/-02-Js-Andrew/Code/async/requests.js
@@ -21,7 +21,6 @@ const getCountryCallbacks = (
   }
   request.send()
 }
-
 // getCountryCallbacks('algeria', console.log)
 
 const getCountryPromise = (countryName) =>
@@ -48,7 +47,8 @@ const getCountryPromise = (countryName) =>
 
 // getCountryPromise('egypt').then(console.log).catch(console.error)
 
-const getCountry = (countryCode) =>
+// using promise chaining
+const getCountryOld = (countryCode) =>
   fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`)
     .then((response) => {
       if (response.ok) {
@@ -61,6 +61,23 @@ const getCountry = (countryCode) =>
     })
     .then((json) => json[0])
 
+// using async/await
+const getCountry = async (countryCode) => {
+  const response = await fetch(
+    `https://restcountries.com/v3.1/alpha/${countryCode}`
+  )
+
+  if (response.ok) {
+    const json = await response.json()
+    return json[0]
+  } else {
+    throw new Error(
+      `Unable to get country with code "${countryCode}"\nStatus: ${response.status} | ${response.statusText}`
+    )
+  }
+}
+
+// show country flag on html
 const showCountryFlag = (name) =>
   getCountry(name)
     .then((country) => {
@@ -86,19 +103,23 @@ const getIPInfo = (token = 'a4b7b3cefd793a') =>
     }
   })
 
-// get ip info then show country flag
-getIPInfo()
-  .then((info) => {
-    return info.country
-  })
-  .then(showCountryFlag)
-  .catch(console.error)
-
 // using async/await
 const showIP = async () => {
   const info = await getIPInfo()
   console.log(`async/await: ${info.ip}`)
   return info
 }
+showIP()
 
-console.log(showIP())
+const getCountryCode = async (token = 'a4b7b3cefd793a') => {
+  const response = await fetch(`https://ipinfo.io/json?token=${token}`)
+  if (response.ok) {
+    const json = await response.json()
+    return json.country
+  } else {
+    throw new Error(`Status ${response.status} | ${response.statusText}`)
+  }
+}
+
+// get ip info then show country flag
+getCountryCode().then(showCountryFlag).catch(console.error)
