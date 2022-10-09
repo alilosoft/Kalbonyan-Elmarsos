fb037d1c3f113b4420fab04882f8f18979f23822
Ali Fellahi <fellahi.ali@gmail.com>
2022-10-09 12:22:36 +0100

    #6 clean boilerplate project

--- 02-Udemy/-02-Js-Andrew/Code/boilerplate/src/challenge.js
+++
@@ -1,9 +0,0 @@
-console.log('challenge.js')
-
-const allCaps = (word) =>
-  word
-    .split('')
-    .map((l) => l.toUpperCase())
-    .join('')
-
-export default allCaps
--- 02-Udemy/-02-Js-Andrew/Code/boilerplate/src/utils.js
+++
@@ -1,7 +0,0 @@
-const add = (a, b) => a + b
-const power = (x, y) => x ** y
-
-console.log('log from my code!')
-
-const square = (x) => x * x
-export { add, power, square as default }
